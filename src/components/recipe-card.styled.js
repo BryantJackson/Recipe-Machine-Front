c24bdae05ada5eea1fce17c92eb0b9124e6ac579
BryantJackson <this.emaill.is.cooll@gmail.com>
2021-09-26 21:40:18 -0600

    Finished Style for Recipe Card

--- src/components/recipe-card.styled.js
+++ src/components/recipe-card.styled.js
@@ -4,13 +4,20 @@ import {FiPlus, FiMinus} from 'react-icons/fi'
 
 
 const Container = styled.div`
+    max-width: 880px;
+
     display: grid;
-    grid-template-columns: 0.9fr;
-    grid-template-rows: 0.2fr 1.9fr; 
-    color : blue;
+    grid-template-columns: 1fr;
+    grid-template-rows: 0.2fr 2.5fr; 
 
     grid-template-areas: "title" "content";
 `;
+
+const Heading  = styled.h4`
+    padding-left: 25px;
+    line-height: 0%;
+    margin-bottom: 0px;
+`
 
 const TitleArea = styled.div`
     background-color: darkgray;
@@ -23,6 +30,7 @@ const TitleArea = styled.div`
     font-size: 20px;
     font-family: Arial, Helvetica, sans-serif;
     line-height: 10%;
+    white-space: nowrap;
 
     grid-area: "title";
     grid-template-columns: 1fr 1fr;
@@ -36,48 +44,69 @@ const TitleArea = styled.div`
 `;
 
 const Content = styled.div`
-    color: black;
     background-color: lightgray;
-    display: grid;
+    border-radius: 5px;
 
+    display: grid;
     grid-template-columns: 1fr 1fr;
-    grid-template-rows: 2fr 1fr;
-    grid-column-gap: 20px;
+    grid-template-rows: 1.8fr 0.25fr;
+    grid-column-gap: 10px;
+    
 `;
 
 const ImgContainer = styled.div`
     display: flex;
-    flex: 0 1 5%
+    flex-direction: column;
+
+    margin-right: 10px;
+    margin-top: 10px;
+    min-height: 96%;
     
     justify-content: end;
-    background-color: whitesmoke;
+    background-image: url("https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F4019381.jpg");
+    background-size: cover;
+    
 
     img {
-        width: 50%;
+        width: 100%;
         height: auto;
-        margin-top: 3%;
-        margin-right: 3%;
+    }
+
+    div {
+        background-color: #5d5555b5;
+        border-radius: 5px;
+        color: whitesmoke;
+        box-shadow: 0 4px 8px 0 rgb(0 0 0 / 20%), 0 6px 20px 0 rgb(0 0 0 / 19%);   
     }
 `
 const RecipeText = styled.div`
     background-color: whitesmoke;
     border-radius: 5px;
     margin: 10px;
+    margin-bottom: 0px;
     padding-right: 25px;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 
-    .bottom {
-        grid-column: 1 / -1 ;
+    li {
+        padding-bottom: 10px;
     }
 `
 const RecipeTextBottom = styled.div`
+
     background-color: whitesmoke;
     border-radius: 5px;
     margin: 10px;
-    padding-right: 25px;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 
     grid-column: 1 / -1 ;
+    
+    ul {
+        column-count: 2;
+    }
+
+    li {
+        padding-bottom: 5px;
+    }
     
 `
 
@@ -95,26 +124,27 @@ function StyledRecipeCard(props) {
             </TitleArea>
             <Content>
                 <RecipeText>
+                    <Heading>Directions:</Heading>
                     <ol>
                         {recipe.directions.map((direct) => (
                                     <li>{direct}</li>
                                 ))}
                     </ol>
                 </RecipeText>
-                <div>
-                    <ImgContainer>
-                        <RecipeText>
-                            <ul>
-                                <li>Calories: {recipe.calories}</li>
-                                <li>Prep Time: {recipe.prep}</li>
-                                <li>Cook Time: {recipe.cook}</li>
-                                <li>Ready In: {recipe["ready in"]}</li>
-                            </ul>
-                        </RecipeText>
-                        <img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F4019381.jpg" />
-                    </ImgContainer>
-                </div>
+                <ImgContainer>
+                    {/* <img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F4019381.jpg" /> */}
+                    <div>
+                        <Heading>Info:</Heading>
+                        <ul>
+                            <li>Calories: {recipe.calories}</li>
+                            <li>Prep Time: {recipe.prep}</li>
+                            <li>Cook Time: {recipe.cook}</li>
+                            <li>Ready In: {recipe["ready in"]}</li>
+                        </ul>
+                    </div>
+                </ImgContainer>
                 <RecipeTextBottom>
+                    <Heading>Ingredients:</Heading>
                     <ul>
                         {recipe.ingredients.map((ing) => (
                                         <li>{ing}</li>
